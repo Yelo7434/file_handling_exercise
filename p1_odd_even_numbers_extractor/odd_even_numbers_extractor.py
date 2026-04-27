c66698d7037764416468b92bc5c3ffc08157b51f
unknown <hernandezrichtericefrancisco@gmail.com>
2026-04-27 17:46:46 +0800

    Add write_file method for date persistence

--- p1_odd_even_numbers_extractor/odd_even_numbers_extractor.py
+++ p1_odd_even_numbers_extractor/odd_even_numbers_extractor.py
@@ -2,4 +2,17 @@ class EvenOddNumbersExtractor:
     def _init_(self, filename: str = "./numbers.txt"):
         self.flame = filename
 
-    
+    def read_file(self) -> list[int]:
+        try:
+            with open(self.flame, "r") as file:
+                numbers = [init(number.rstrip("\n")) for number in file.readlines()]
+                return numbers
+            
+        except:
+            print("File must only contain integers")
+
+    def write_file(self, filename: str, content: int):
+        with open(filenamer, "w") as file:
+            file.write(str(content) + "\n")
+
+            
