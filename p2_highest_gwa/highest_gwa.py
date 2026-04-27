3171dc15040b6b21453c1c0aeb134d5967f3c5f6
unknown <hernandezrichtericefrancisco@gmail.com>
2026-04-27 19:12:32 +0800

    file reading

--- p2_highest_gwa/highest_gwa.py
+++ p2_highest_gwa/highest_gwa.py
@@ -1,4 +1,9 @@
-class HighestGwa:
+class HighestGWA:
     def __init__(self, filename: str = "gwa.txt"):
         self.filename = filename
+
+    def read_file(self) -> list[str]:
+        with open(self.filename, "r") as file:
+            content = [line.rstrip("\n") for line in file]
+        return content 
         
