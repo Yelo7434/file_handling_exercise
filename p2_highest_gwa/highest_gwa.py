acdafc878ae9d01ca4dbd84656da3229df657b35
unknown <hernandezrichtericefrancisco@gmail.com>
2026-04-27 19:16:21 +0800

    add get_highest method to process data and find top student

--- p2_highest_gwa/highest_gwa.py
+++ p2_highest_gwa/highest_gwa.py
@@ -6,4 +6,9 @@ class HighestGWA:
         with open(self.filename, "r") as file:
             content = [line.rstrip("\n") for line in file]
         return content 
+    
+    def get_highest_gwa(self, data: list):
+        new_data = [info.split(",") for info in data]
+        highest_gwa = max(new_data, key = lambda x: x[1])
+        print (f"Highest GWA\nStudent: {highest_gwa[0]}\nGWA: {highest_gwa[1]}")
         
