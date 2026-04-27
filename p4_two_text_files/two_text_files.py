faf398673413f39c93977b1c6dba3fac1bf2f0cc
unknown <hernandezrichtericefrancisco@gmail.com>
2026-04-27 22:49:42 +0800

    add read_file and write_file helper methods

--- p4_two_text_files/two_text_files.py
+++ p4_two_text_files/two_text_files.py
@@ -4,4 +4,15 @@ class TwoFileIntegers:
         self.odd_filename = "triple.txt"
         self.even_filename = "doubpe.txt"
 
-        
+    def read_file(self):
+        try:
+            with open(self.filename, 'r') as file:
+                return file.readlines()
+        except:
+            print("File not found.")
+
+    def write_file(self, filename: str, content: str):
+        with open(filename, "a") as file:
+            file.write(f"{str(content)}\n")
+
+    
