6ee6bf00765dcdfb07cad8a63f91f53ea73ae5a9
unknown <hernandezrichtericefrancisco@gmail.com>
2026-04-27 22:57:38 +0800

    add process_integers for squaring evens and cubing odds

--- p4_two_text_files/two_text_files.py
+++ p4_two_text_files/two_text_files.py
@@ -18,3 +18,13 @@ class TwoFileIntegers:
     def process_files(self, data: list) -> list[int]:
         return [int(line.rstrip("\n")) for line in data]
     
+    def process_integers(self):
+        data = self.read_file()
+        integers = self.process_files(data)
+
+        for number in integers:
+            if number % 2 == 0:
+                self.write_file(self.even_filename, number ** 2)
+            else:
+                self.write_file(self.odd_filename, number ** 3)
+                
