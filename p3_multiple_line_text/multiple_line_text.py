794b5a929a5384cae35c9f97bb6c43d05e8bf0b5
unknown <hernandezrichtericefrancisco@gmail.com>
2026-04-27 22:24:02 +0800

    implement mutiple_line_text method with append logic

--- p3_multiple_line_text/multiple_line_text.py
+++ p3_multiple_line_text/multiple_line_text.py
@@ -2,4 +2,13 @@ class MultipleLineText:
     def __init__(self, filename: str = "mylife.txt"):
         self.filename = filename
 
-        
+    def multiple_line_text(self):
+        try:
+            with open(self.filename, 'a') as file:
+                while True:
+                    text = input("Enter line: ")
+                    more = input("Are there more lines (y/n)? ")
+                    file.write(text + "\n")
+                    
+                    if more != 'y':
+                        break
