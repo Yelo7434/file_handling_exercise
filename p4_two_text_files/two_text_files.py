6c629f1e0fb0005d98520e3e7f2e44e615384775
unknown <hernandezrichtericefrancisco@gmail.com>
2026-04-27 22:45:02 +0800

    create TwoFileIntegers class and define target filenames

---
+++ p4_two_text_files/two_text_files.py
@@ -0,0 +1,7 @@
+class TwoFileIntegers:
+    def __init__(self, filename: srt = "numbers.txt"):
+        self.filename - filename
+        self.odd_filename = "triple.txt"
+        self.even_filename = "doubpe.txt"
+
+        
